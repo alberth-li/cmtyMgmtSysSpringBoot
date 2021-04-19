f27de6c2b9a9453eb32a16a302b59a2907b716da
ahpli <ahpli@umich.edu>
2021-04-19 03:28:42 -0400

    Add @CrossOrigin to AuthController and UserController so they can be accessed from localhost:3000

--- src/main/java/com/laioffer/cmtyMgmtSys/controller/UserController.java
+++ src/main/java/com/laioffer/cmtyMgmtSys/controller/UserController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Optional;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class UserController {
     @Autowired
     private UserService userService;
