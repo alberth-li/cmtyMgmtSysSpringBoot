31351b88b8a8900b34725c4504d55487acd981bd
ahpli <ahpli@umich.edu>
2021-04-21 05:31:39 -0400

    Delete Scheduler codes.

--- src/main/java/com/laioffer/cmtyMgmtSys/dao/SchedulerRepository.java
+++
@@ -1,8 +0,0 @@
-package com.laioffer.cmtyMgmtSys.dao;
-
-import com.laioffer.cmtyMgmtSys.entity.RoomBooking;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-@Repository
-public interface SchedulerRepository extends JpaRepository<RoomBooking, Long> {
-}
