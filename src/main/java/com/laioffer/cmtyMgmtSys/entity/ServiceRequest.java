451071fff6e9e4b591614faa3e1d2dbb0fd2066b
ahpli <ahpli@umich.edu>
2021-04-20 23:23:29 -0400

    Add full support for servicerequest API

--- src/main/java/com/laioffer/cmtyMgmtSys/entity/ServiceRequest.java
+++ src/main/java/com/laioffer/cmtyMgmtSys/entity/ServiceRequest.java
@@ -1,5 +1,7 @@
 package com.laioffer.cmtyMgmtSys.entity;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -11,7 +13,9 @@ import java.util.Date;
 @Entity
 @Table(name = "servicerequest")
 @EntityListeners(AuditingEntityListener.class)
-public class ServiceRequest implements Serializable {
+@Setter
+@Getter
+public class ServiceRequest extends Auditable<Long> implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
@@ -22,85 +26,18 @@ public class ServiceRequest implements Serializable {
     @Enumerated(EnumType.STRING)
     private ServiceRequestType type;
 
-    private String message;
+    private String description;
 
-    @Column(nullable = false, updatable = false)
+    private boolean disabled;
+
     @Temporal(TemporalType.TIMESTAMP)
-    @CreatedDate
-    private Date timeCreated;
+    private Date desiredServiceTime;
 
     @Column(updatable = false)
     @Temporal(TemporalType.TIMESTAMP)
     private Date timeCompleted;
 
     @ManyToOne
-    @JoinColumn(name = "apt_ID")
+    @JoinColumn(name = "apt_Id")
     private Apartment apt;
-
-    @CreatedBy
-    private Resident requester;
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public ServiceRequestStatus getStatus() {
-        return status;
-    }
-
-    public void setStatus(ServiceRequestStatus status) {
-        this.status = status;
-    }
-
-    public ServiceRequestType getType() {
-        return type;
-    }
-
-    public void setType(ServiceRequestType type) {
-        this.type = type;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    public Date getTimeCreated() {
-        return timeCreated;
-    }
-
-    public void setTimeCreated(Date timeCreated) {
-        this.timeCreated = timeCreated;
-    }
-
-    public Date getTimeCompleted() {
-        return timeCompleted;
-    }
-
-    public void setTimeCompleted(Date timeCompleted) {
-        this.timeCompleted = timeCompleted;
-    }
-
-    public Apartment getApt() {
-        return apt;
-    }
-
-    public void setApt(Apartment apt) {
-        this.apt = apt;
-    }
-
-    public Resident getRequester() {
-        return requester;
-    }
-
-    public void setRequester(Resident requester) {
-        this.requester = requester;
-    }
 }
